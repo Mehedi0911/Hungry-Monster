16413820e282f3a64c2c91d9d12a9168b0931f94
unknown <mmraaj378@gmail.com>
2021-02-07 20:09:24 +0600

    some update

--- app.js
+++ app.js
@@ -65,24 +65,6 @@ const showMealInfo = meal => {
 }
 
 
-// newMeal.addEventListener('click', function(){
-//     mealInfo = `<div>
-//     <img src=${meal.strMealThumb}>
-//     <h5>${meal.strMeal}</h5>
-//     <h6>Ingredients</h6>
-//     <p>${meal.strMeasure1 + " " + meal.strIngredient1}</P>
-//     <p>${meal.strMeasure2 + " " + meal.strIngredient2}</P>
-//     <p>${meal.strMeasure3 + " " + meal.strIngredient3}</P>
-//     <p>${meal.strMeasure4 + " " + meal.strIngredient4}</P>
-//     <p>${meal.strMeasure5 + " " + meal.strIngredient5}</P>
-//     <p>${meal.strMeasure6 + " " + meal.strIngredient6}</P>
-//     <p>${meal.strMeasure7 + " " + meal.strIngredient7}</P>
-//     <p>${meal.strMeasure8 + " " + meal.strIngredient8}</P>
-//     <p>${meal.strMeasure9 + " " + meal.strIngredient9}</P>
-//     <p>${meal.strMeasure10 + " " + meal.strIngredient10}</P>
 
-//     </div>`
-//     newMeal.innerHTML = mealInfo;
-// })
 
 
