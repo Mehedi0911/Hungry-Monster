9db6eea844c4b197030d1dafcc69b2080e56ea42
unknown <mmraaj378@gmail.com>
2021-02-07 20:06:38 +0600

    details update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ searchBtn.addEventListener('click', function(e){
     const mealInputFirstLeter = mealInput.charAt(0);
     console.log(mealInputFirstLeter);
 
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?f='+mealInputFirstLeter+'')
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?f='+mealInputFirstLeter+'') //getting the filtered url
 .then(response => response.json())
 .then(data => displayMeals(data.meals));
 
@@ -14,34 +14,18 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?f='+mealInputFirstLete
     mealContainer.innerHTML = "";
     meals.forEach(meal => {
         console.log(meal);
-        const newMeal = document.createElement('div');
+        const newMeal = document.createElement('div'); // creating new div items for each meal items
         let mealInfo = `<div>
              <img src=${meal.strMealThumb}>
-             <h6>${meal.strMeal}</h6>
+             <h5>${meal.strMeal}</h5>
              </div>`
         newMeal.innerHTML = mealInfo;
         mealContainer.appendChild(newMeal);
 
-        newMeal.addEventListener('click', function(){
-            mealInfo = `<div>
-            <img src=${meal.strMealThumb}>
-            <h5>${meal.strMeal}</h5>
-            <h6>Ingredients</h6>
-            <p>${meal.strMeasure1 + " " + meal.strIngredient1}</P>
-            <p>${meal.strMeasure2 + " " + meal.strIngredient2}</P>
-            <p>${meal.strMeasure3 + " " + meal.strIngredient3}</P>
-            <p>${meal.strMeasure4 + " " + meal.strIngredient4}</P>
-            <p>${meal.strMeasure5 + " " + meal.strIngredient5}</P>
-            <p>${meal.strMeasure6 + " " + meal.strIngredient6}</P>
-            <p>${meal.strMeasure7 + " " + meal.strIngredient7}</P>
-            <p>${meal.strMeasure8 + " " + meal.strIngredient8}</P>
-            <p>${meal.strMeasure9 + " " + meal.strIngredient9}</P>
-            <p>${meal.strMeasure10 + " " + meal.strIngredient10}</P>
-        
-            </div>`
-            newMeal.innerHTML = mealInfo;
-        })
-
+        // Individual item Ingredients click event handler.
+        newMeal.onclick = function(){
+            displayIngredients(meal.strMeal);
+        }
         
     });
     
@@ -50,7 +34,55 @@ fetch('https://www.themealdb.com/api/json/v1/1/search.php?f='+mealInputFirstLete
     
 })
 
+const displayIngredients = mealName =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => showMealInfo(data.meals[0]));
+}
+
+const showMealInfo = meal => {
+    const mealDetails = document.getElementById('mealDetails');
+
+    mealDetails.innerHTML = `<div>
+         <img src=${meal.strMealThumb}>
+         <h5>${meal.strMeal}</h5>
+         <h6>Ingredients</h6>
+         <p>${meal.strMeasure1 + " " + meal.strIngredient1}</P>
+         <p>${meal.strMeasure2 + " " + meal.strIngredient2}</P>
+         <p>${meal.strMeasure3 + " " + meal.strIngredient3}</P>
+         <p>${meal.strMeasure4 + " " + meal.strIngredient4}</P>
+         <p>${meal.strMeasure5 + " " + meal.strIngredient5}</P>
+         <p>${meal.strMeasure6 + " " + meal.strIngredient6}</P>
+         <p>${meal.strMeasure7 + " " + meal.strIngredient7}</P>
+         <p>${meal.strMeasure8 + " " + meal.strIngredient8}</P>
+         <p>${meal.strMeasure9 + " " + meal.strIngredient9}</P>
+         <p>${meal.strMeasure10 + " " + meal.strIngredient10}</P>
+    
+         </div>
+   
+    `
+}
 
 
+// newMeal.addEventListener('click', function(){
+//     mealInfo = `<div>
+//     <img src=${meal.strMealThumb}>
+//     <h5>${meal.strMeal}</h5>
+//     <h6>Ingredients</h6>
+//     <p>${meal.strMeasure1 + " " + meal.strIngredient1}</P>
+//     <p>${meal.strMeasure2 + " " + meal.strIngredient2}</P>
+//     <p>${meal.strMeasure3 + " " + meal.strIngredient3}</P>
+//     <p>${meal.strMeasure4 + " " + meal.strIngredient4}</P>
+//     <p>${meal.strMeasure5 + " " + meal.strIngredient5}</P>
+//     <p>${meal.strMeasure6 + " " + meal.strIngredient6}</P>
+//     <p>${meal.strMeasure7 + " " + meal.strIngredient7}</P>
+//     <p>${meal.strMeasure8 + " " + meal.strIngredient8}</P>
+//     <p>${meal.strMeasure9 + " " + meal.strIngredient9}</P>
+//     <p>${meal.strMeasure10 + " " + meal.strIngredient10}</P>
+
+//     </div>`
+//     newMeal.innerHTML = mealInfo;
+// })
 
 
